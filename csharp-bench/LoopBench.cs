c66b5dedd4de6289546fe0b649b3560e90a1f7b6
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2025-09-07 15:09:46 -0300

    update: add csharp bench

---
+++ csharp-bench/LoopBench.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Diagnostics;
+
+class LoopBench
+{
+    const long N = 100_000_000;
+
+    static void Main()
+    {
+        var sw = Stopwatch.StartNew();
+        long sum = 0;
+        for (long i = 0; i < N; i++)
+        {
+            sum += i;
+        }
+        sw.Stop();
+        Console.WriteLine($"result: {sum}");
+        Console.WriteLine($"elapsed time: {sw.ElapsedMilliseconds} ms");
+    }
+}
